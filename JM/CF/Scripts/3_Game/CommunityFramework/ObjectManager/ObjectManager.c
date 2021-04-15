d98685329f61c132f50de9adc88ef21ab7491dd7
Jacob-Mango <mangosprograms@gmail.com>
2021-04-15 16:34:53 +1000

    Added helper methods to register and unregister objects without hiding

--- JM/CF/Scripts/3_Game/CommunityFramework/ObjectManager/ObjectManager.c
+++ JM/CF/Scripts/3_Game/CommunityFramework/ObjectManager/ObjectManager.c
@@ -2,17 +2,87 @@ class CF_ObjectManager_ObjectLink extends OLinkT
 {
     int flags;
     int eventMask;
-}
+    bool hidden;
+};
 
 class CF_ObjectManager
 {
     //!Single static instance. Do not create with new or spawn - use CF.ObjectManager for access instead.
-    protected void CF_ObjectManager();
-    protected void ~CF_ObjectManager();
+    private void CF_ObjectManager();
+    private void ~CF_ObjectManager();
 
     protected static const int HIDE_OBJECT_AXIS_OFFSET = 10000;
 
-    protected static const ref map<Object, ref CF_ObjectManager_ObjectLink> m_HiddenObjects = new map<Object, ref CF_ObjectManager_ObjectLink>();
+    protected static const ref map<Object, ref CF_ObjectManager_ObjectLink> m_RegisteredObjects = new map<Object, ref CF_ObjectManager_ObjectLink>();
+
+    //!Storing as an array because GetHiddenMapObjects() would be O(n^3) in complexity
+    protected static const ref array<CF_ObjectManager_ObjectLink> m_RegisteredObjectsArray = new array<CF_ObjectManager_ObjectLink>();
+
+	/**
+	 * @brief Registers obj to be exempt of being affected by the network bubble and the view distance.
+	 *
+	 * @param obj	Object pointer.
+	 * @return bool	true of the object registered, false otherwise.
+	 */
+    static CF_ObjectManager_ObjectLink RegisterObject(Object object)
+    {
+        // Check to see if the object is already linked
+        auto link = m_RegisteredObjects[object];
+        if (link) return link;
+
+        link = new CF_ObjectManager_ObjectLink(object);
+        link.flags = object.GetFlags();
+        link.eventMask = object.GetEventMask();
+        link.hidden = false;
+
+        m_RegisteredObjectsArray.Insert(link);
+        m_RegisteredObjects.Set(object, link);
+
+        return link;
+    }
+
+	/**
+	 * @brief Unregisters obj so that it can be affected by the network bubble and the view distance.
+     * @note Doesn't delete the link
+	 *
+	 * @param obj	Object pointer.
+	 * @return bool	true of the object unregistered, false otherwise.
+	 */
+    static CF_ObjectManager_ObjectLink _UnregisterObject(Object object)
+    {
+        //Remove object from hidden collection
+        auto link = m_RegisteredObjects[object];
+
+        if (!link) return NULL; //Object not known as hidden
+
+        m_RegisteredObjects.Remove(object);
+        
+        int idx = m_RegisteredObjectsArray.Find(link);
+        if (idx != -1) m_RegisteredObjectsArray.Remove(idx);
+        
+        return link;
+    }
+
+	/**
+	 * @brief Unregisters obj so that it can be affected by the network bubble and the view distance.
+	 *
+	 * @param obj	Object pointer.
+	 * @return bool	true of the object unregistered, false otherwise.
+	 */
+    static void UnregisterObject(Object object)
+    {
+        //Remove object from hidden collection
+        auto link = m_RegisteredObjects[object];
+
+        if (!link) return; //Object not known as hidden
+
+        m_RegisteredObjects.Remove(object);
+        
+        int idx = m_RegisteredObjectsArray.Find(link);
+        if (idx != -1) m_RegisteredObjectsArray.Remove(idx);
+
+        delete link;
+    }
 
     /**
      * @brief Hides a static map object (Houses, Vegetation, etc.) visually and physically.
@@ -34,14 +104,12 @@ class CF_ObjectManager
         auto originalPosition = object.GetPosition();
 
         //Register object in it's current state
-        auto link = new CF_ObjectManager_ObjectLink(object);
-        link.flags = object.GetFlags();
-        link.eventMask = object.GetEventMask();
+        auto link = RegisterObject(object);
+
+        link.hidden = true;
 
         object.ClearFlags( link.flags, true );
         object.ClearEventMask( link.eventMask );
-
-        m_HiddenObjects.Set(object, link);
 
         vector tm[4];
         object.GetTransform(tm);
@@ -145,11 +213,9 @@ class CF_ObjectManager
     static Object UnhideMapObject(Object object, bool updatePathGraph = true)
     {
         //Remove object from hidden collection
-        auto link = m_HiddenObjects[object];
+        auto link = _UnregisterObject(object);
 
-        if (!link) return NULL; //Object not known as hidden
-
-        m_HiddenObjects.Remove(object);
+        if (!link || !link.hidden) return NULL; //Object not known as hidden
 
         vector tm[4];
         object.GetTransform(tm);
@@ -169,6 +235,8 @@ class CF_ObjectManager
             auto radiusVector = Vector(objectRadius, objectRadius, objectRadius);
             g_Game.UpdatePathgraphRegion(objectPosition - radiusVector, objectPosition + radiusVector);
         }
+
+        delete link;
 
         return object;
     }
@@ -240,9 +308,9 @@ class CF_ObjectManager
 	{
         array<Object> unhidden();
 
-		for (int nObject = 0; nObject < m_HiddenObjects.Count(); nObject++)
+		for (int nObject = 0; nObject < m_RegisteredObjects.Count(); nObject++)
 		{
-            auto object = m_HiddenObjects.GetKey(nObject);
+            auto object = m_RegisteredObjects.GetKey(nObject);
             
             //Todo potential improvement -> s. HideMapObjects
             if (UnhideMapObject(object, updatePathGraph))
@@ -264,7 +332,32 @@ class CF_ObjectManager
      */
 	static array<Object> GetHiddenMapObjects()
 	{
-        return m_HiddenObjects.GetKeyArray();
+		ref array<Object> objects = new array<Object>;
+		for (int i = 0; i < m_RegisteredObjectsArray.Count(); i++)
+		{
+            auto link = m_RegisteredObjectsArray[i];
+            if (link.hidden) objects.Insert(link.Ptr());
+		}
+		return objects;
+	}
+
+    /**
+     * @brief Returns all map objects that are currently hidden
+     * @code
+     * array<Object> objects = CF.ObjectManager.GetHiddenMapObjects();
+     * @endcode
+     *
+     * @return array<Object> Array of hidden objects.
+     */
+	static array<Object> GetRegisteredObjects()
+	{
+		ref array<Object> objects = new array<Object>;
+		for (int i = 0; i < m_RegisteredObjectsArray.Count(); i++)
+		{
+            auto link = m_RegisteredObjectsArray[i];
+            objects.Insert(link.Ptr());
+		}
+		return objects;
 	}
 
     /**
@@ -278,7 +371,23 @@ class CF_ObjectManager
      */
     static bool IsMapObjectHidden(Object object)
     {
-        return m_HiddenObjects.Contains(object));
+        CF_ObjectManager_ObjectLink link;
+        if (!m_RegisteredObjects.Find(object, link)) return false;
+        return link.hidden;
+    }
+
+    /**
+     * @brief Checks if a map object is registered
+     * @code
+     * bool isRegistered = CF.ObjectManager.IsObjectRegistered(object);
+     * @endcode
+     *
+     * @param object Object to check
+     * @return bool true if currently registered, false otherwise.
+     */
+    static bool IsObjectRegistered(Object object)
+    {
+        return m_RegisteredObjects.Contains(object);
     }
 
     /**
@@ -307,7 +416,10 @@ class CF_ObjectManager
     static void _Cleanup()
     {
         //Cleanup hidden object allocation
-        m_HiddenObjects.Clear();
-        delete m_HiddenObjects;
+        m_RegisteredObjects.Clear();
+        m_RegisteredObjectsArray.Clear();
+
+        delete m_RegisteredObjects;
+        delete m_RegisteredObjectsArray;
     }
-}
+};
